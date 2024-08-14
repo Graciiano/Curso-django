81541fcbff02818fc7f5f9e4ac2b3fde29aeb41f
Diogo Graciano <diogodev00@gmail.com>
2024-08-14 07:10:57 -0300

    removed contato and sobre

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from core.views import home, contact, sobre
+from core.views import home
 
 urlpatterns = [
     path('', home),
-    path('contato', contact),
-    path('sobre', sobre),
 ]
--- core/views.py
+++ core/views.py
@@ -1,14 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Diogo Graciano'
     })
-
-def contact(request):
-    return HttpResponse('CONTATO')
-
-def sobre(request):
-    return HttpResponse('SOBRE')
